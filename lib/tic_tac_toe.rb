22e9a7974ae809fa4c742a1c5b2b01ccb9f5ab3d
Kevin Randles <krandles@gmail.com>
2017-11-05 20:06:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,4 +40,14 @@ class TicTacToe
     end
   end
 
+  def turn_count
+    counter = 0
+    @board.each do |cell|
+      if cell != " "
+        counter += 1
+      end
+    end
+    counter
+  end
+
 end
